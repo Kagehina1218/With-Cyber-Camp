55ced84d022dd495f88b3ac17d14a3bd826ab693
Kagehina1218 <67077298+Kagehina1218@users.noreply.github.com>
2025-07-21 20:00:15 -0700

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -2,14 +2,10 @@
 
 echo "Updating package list..."
 sudo apt update
-
 echo "Installing tcpdump..."
 sudo apt install -y tcpdump
-
 echo "Installing nmap..."
 sudo apt install -y nmap
-
 echo "Installing Wireshark..."
 sudo apt install -y wireshark
-
 echo "All tools installed."
